a161c22111e5c227f3da68c6f6fa182692d7f266
Ivonne <ivonnebenites2015@gmail.com>
2024-02-13 11:27:01 -0500

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import AddPlace from './routes/AddPlace';
 import DeletePlace from './routes/DeletePlace';
 import MyReservations from './routes/MyReservations';
 import Reserve from './routes/Reserve';
-import Places from './routes/Places';
+
 import NotMatch from './routes/NotMatch';
 import Layout from './components/Layout';
+import LogIn from './routes/LogIn';
 
 import PlaceDetails from './routes/PlaceDetails';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<Places />} />
+
       <Route path="/places/:id" element={<PlaceDetails />} />
       <Route path="addplace" element={<AddPlace />} />
       <Route path="deleteplace" element={<DeletePlace />} />
@@ -21,6 +22,7 @@ const App = () => (
       <Route path="reserve" element={<Reserve />} />
       <Route path="*" element={<NotMatch />} />
     </Route>
+    <Route path="login" element={<LogIn />} />
 
   </Routes>
 
